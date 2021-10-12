fed994ddaf2ed9f4353867e1f6fdfb3aa9e3dd93
MayNiklas <info@niklas-steffen.de>
2021-10-12 14:38:31 +0200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,109 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager/release-21.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    mayniklas.url = "github:mayniklas/nixos";
+    mayniklas.inputs.nixpkgs.follows = "nixpkgs";
+    mayniklas.inputs.home-manager.follows = "home-manager";
+
+  };
+
+  outputs = { self, ... }@inputs:
+    with inputs;
+    let
+      # Function to create defult (common) system config options
+      defFlakeSystem = systemArch: baseCfg:
+        nixpkgs.lib.nixosSystem {
+          system = "${systemArch}";
+          modules = [
+            # Add home-manager option to all configs
+            ({ ... }: {
+              imports = builtins.attrValues self.nixosModules
+                ++ builtins.attrValues mayniklas.nixosModules ++ [
+                  { # Set the $NIX_PATH entry for nixpkgs. This is necessary in
+                    # this setup with flakes, otherwise commands like `nix-shell
+                    # -p pkgs.htop` will keep using an old version of nixpkgs.
+                    # With this entry in $NIX_PATH it is possible (and
+                    # recommended) to remove the `nixos` channel for both users
+                    # and root e.g. `nix-channel --remove nixos`. `nix-channel
+                    # --list` should be empty for all users afterwards
+                    nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
+                    nixpkgs.overlays = [ self.overlay self.overlay-unstable ];
+                  }
+                  baseCfg
+                  home-manager.nixosModules.home-manager
+                  # DONT set useGlobalPackages! It's not necessary in newer
+                  # home-manager versions and does not work with configs using
+                  # `nixpkgs.config`
+                  { home-manager.useUserPackages = true; }
+                ];
+              # Let 'nixos-version --json' know the Git revision of this flake.
+              system.configurationRevision =
+                nixpkgs.lib.mkIf (self ? rev) self.rev;
+              nix.registry.nixpkgs.flake = nixpkgs;
+            })
+          ];
+        };
+
+    in {
+
+      # Expose overlay to flake outputs, to allow using it from other flakes.
+      overlay = final: prev: (import ./overlays) final prev;
+
+      overlay-unstable = final: prev: {
+        unstable = import nixpkgs-unstable {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+      };
+
+      # Output all modules in ./modules to flake. Modules should be in
+      # individual subdirectories and contain a default.nix file
+      nixosModules = builtins.listToAttrs (map (x: {
+        name = x;
+        value = import (./modules + "/${x}");
+      }) (builtins.attrNames (builtins.readDir ./modules)));
+
+      # Each subdirectory in ./machins is a host. Add them all to
+      # nixosConfiguratons. Host configurations need a file called
+      # configuration.nix that will be read first
+      nixosConfigurations = builtins.listToAttrs (map (x: {
+        name = x;
+        value = defFlakeSystem "x86_64-linux" {
+          imports = [
+            (import (./machines + "/${x}/configuration.nix") { inherit self; })
+          ];
+        };
+      }) (builtins.attrNames (builtins.readDir ./machines)));
+    } //
+
+    (flake-utils.lib.eachSystem [ "i686-linux" "x86_64-linux" ]) (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+          config = {
+            allowUnsupportedSystem = true;
+            allowUnfree = true;
+          };
+        };
+      in rec {
+
+        packages =
+          flake-utils.lib.flattenTree { bukkit-spigot = pkgs.bukkit-spigot; };
+
+        apps = {
+          # Allow custom packages to be run using `nix run`
+          bukkit-spigot =
+            flake-utils.lib.mkApp { drv = packages.bukkit-spigot; };
+        };
+      });
+}
