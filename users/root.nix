560c94aa7e1bd279fca8f6728fdd70f2ab80ecd8
MayNiklas <info@niklas-steffen.de>
2021-10-12 17:00:59 +0200

    add lgoette.keys to root

--- users/root.nix
+++ users/root.nix
@@ -3,8 +3,12 @@
   users.users.root = {
     openssh.authorizedKeys.keyFiles = [
       (pkgs.fetchurl {
-        url = "https://github.com/mayniklas.keys";
-        sha256 = "sha256-47KOHBE0eYZxHt9ENNNiD97jbBHeNq5lc83lOUFfjZw=";
+        url = "https://github.com/MayNiklas.keys";
+        sha256 = "174dbx0kkrfdfdjswdny25nf7phgcb9k8i6z3rqqcy9l24f8xcp3";
+      })
+      (pkgs.fetchurl {
+        url = "https://github.com/lgoette.keys";
+        sha256 = "0m1vnwrnxvzas9djnychnr2a67zknpac1bfpwmnd07n9gr3x3lha";
       })
     ];
   };
