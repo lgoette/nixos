0d708663003a4d96144c8652687c0993ea97c89e
MayNiklas <info@niklas-steffen.de>
2022-04-06 23:42:03 +0200

    add webserver

--- packages/minecraft-controller/minecraft-controller.go
+++ packages/minecraft-controller/minecraft-controller.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello, World!")
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	http.HandleFunc("/", handler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
