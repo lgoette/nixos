9833a01429a02f64a14021007dcc0257ff7976d4
MayNiklas <info@niklas-steffen.de>
2022-05-03 18:34:41 +0200

    add unifi package to flake output

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,4 +5,8 @@ self: super: {
   minecraft-controller =
     super.pkgs.callPackage ../packages/minecraft-controller { };
 
+  inherit (super.pkgs.callPackages ../packages/unifi { })
+    unifiLTS unifi5 unifi6 unifi7;
+  unifi = super.pkgs.unifi7;
+
 }
