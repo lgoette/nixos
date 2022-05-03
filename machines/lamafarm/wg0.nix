fc03c37f61b609fd456046f7b4f616f6c4b19d0e
MayNiklas <info@niklas-steffen.de>
2022-05-03 09:11:22 +0200

    prepare persistentKeepalive

--- machines/lamafarm/wg0.nix
+++ machines/lamafarm/wg0.nix
@@ -39,60 +39,70 @@ in {
         {
           publicKey = "CQ0eGmaUXekRdYxA45pbmzkcCb5w5Rf9fKV4VkwcBmo=";
           allowedIPs = [ "10.11.12.5" "192.168.176.0/24" ];
+          # persistentKeepalive = 15;
         }
 
         # 10.11.12.6
         {
           publicKey = "fjL1uv+PF1ajKzJt0CDh9LCofPYAtGdqVOWRbC62eC4=";
           allowedIPs = [ "10.11.12.6" "192.168.178.0/24" ];
+          # persistentKeepalive = 15;
         }
 
         # 10.11.12.8
         {
           publicKey = "5KIvDXqEpt/bDd0pao0CrnaZTeZtYBtGq//bSjHScDY=";
           allowedIPs = [ "10.11.12.8" ];
+          # persistentKeepalive = 15;
         }
 
         # 10.11.12.100
         {
           publicKey = "07/aJQKEziJdj6UTpbUNWAEEnpugtZ61stoM29sfIGQ=";
           allowedIPs = [ "10.11.12.100" ];
+          # persistentKeepalive = 15;
         }
 
         # 10.11.12.101
         {
           publicKey = "dOXyHemzCQJlw0L3j9P5Ue4SrdttJi8k7/Q1ifK4HhY=";
           allowedIPs = [ "10.11.12.101" ];
+          # persistentKeepalive = 15;
         }
 
         # 10.11.12.102                                                                             
         {
           publicKey = "sISbItjlwv9VCe5ZmOFa8irsMmiDgj7cy+aC+Do7mRQ=";
           allowedIPs = [ "10.11.12.102" ];
+          # persistentKeepalive = 15;
         }
 
         # 10.11.12.203
         {
           publicKey = "qfStJLiGXGuK5Gh80XE09ceY7f75/tTDA0sTVSZqGA4=";
           allowedIPs = [ "10.11.12.203" ];
+          # persistentKeepalive = 15;
         }
 
         # 10.11.12.204
         {
           publicKey = "tP8n9Ux+wb+Z3laLvGQ0Y6n68aw/wggvA2VoVyf8HE8=";
           allowedIPs = [ "10.11.12.204" "192.168.78.0/24" ];
+          # persistentKeepalive = 15;
         }
 
         # 10.11.12.223
         {
           publicKey = "C5I0d+OEkdlyMQbQqW2Peg12ZqO+G4jhxIvnZk11wTQ=";
           allowedIPs = [ "10.11.12.223" ];
+          # persistentKeepalive = 15;
         }
 
         # 10.11.12.224
         {
           publicKey = "bo0Y3aXF3ee61C8GWA18z+inAdYzNTsqUqrjRaR672E=";
           allowedIPs = [ "10.11.12.224" ];
+          # persistentKeepalive = 15;
         }
 
       ];
