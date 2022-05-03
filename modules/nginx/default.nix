dffda838769977769d4fa73d5d63ec9c03355b65
MayNiklas <info@niklas-steffen.de>
2022-05-03 17:07:00 +0200

    only allow Cloudflare

--- modules/nginx/default.nix
+++ modules/nginx/default.nix
@@ -32,6 +32,35 @@ in {
             "/var/src/secrets/ssl/cf_workshop.lasse-goette.de.pem";
           addSSL = true;
           root = "/var/www/workshop.lasse-goette.de";
+          extraConfig = ''
+            # https://www.cloudflare.com/ips
+            # IPv4
+            allow 173.245.48.0/20;
+            allow 103.21.244.0/22;
+            allow 103.22.200.0/22;
+            allow 103.31.4.0/22;
+            allow 141.101.64.0/18;
+            allow 108.162.192.0/18;
+            allow 190.93.240.0/20;
+            allow 188.114.96.0/20;
+            allow 197.234.240.0/22;
+            allow 198.41.128.0/17;
+            allow 162.158.0.0/15;
+            allow 104.16.0.0/13;
+            allow 104.24.0.0/14;
+            allow 172.64.0.0/13;
+            allow 131.0.72.0/22;
+            # IPv6
+            allow 2400:cb00::/32;
+            allow 2606:4700::/32;
+            allow 2803:f800::/32;
+            allow 2405:b500::/32;
+            allow 2405:8100::/32;
+            allow 2a06:98c0::/29;
+            allow 2c0f:f248::/32;
+            # Generated at Tue May  3 17:03:29 CEST 2022
+            deny all; # deny all remaining ips
+          '';
         };
 
         "urban-disclaimer.de" = mkIf cfg.urban-disclaimer {
@@ -40,6 +69,35 @@ in {
           sslCertificate = "/var/src/secrets/ssl/cf_urban-disclaimer.de.pem";
           addSSL = true;
           root = "/var/www/urban-disclaimer.de";
+          extraConfig = ''
+            # https://www.cloudflare.com/ips
+            # IPv4
+            allow 173.245.48.0/20;
+            allow 103.21.244.0/22;
+            allow 103.22.200.0/22;
+            allow 103.31.4.0/22;
+            allow 141.101.64.0/18;
+            allow 108.162.192.0/18;
+            allow 190.93.240.0/20;
+            allow 188.114.96.0/20;
+            allow 197.234.240.0/22;
+            allow 198.41.128.0/17;
+            allow 162.158.0.0/15;
+            allow 104.16.0.0/13;
+            allow 104.24.0.0/14;
+            allow 172.64.0.0/13;
+            allow 131.0.72.0/22;
+            # IPv6
+            allow 2400:cb00::/32;
+            allow 2606:4700::/32;
+            allow 2803:f800::/32;
+            allow 2405:b500::/32;
+            allow 2405:8100::/32;
+            allow 2a06:98c0::/29;
+            allow 2c0f:f248::/32;
+            # Generated at Tue May  3 17:03:29 CEST 2022
+            deny all; # deny all remaining ips
+          '';
         };
 
       };
