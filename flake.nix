8b731ab8184f245df73e5d529da2962a0c6d5294
Lasse Götte <git@lasse-goette.de>
2022-12-05 17:05:14 +0100

    change to nixos 22.11

--- flake.nix
+++ flake.nix
@@ -3,12 +3,12 @@
 
   inputs = {
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.05";
+      url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
