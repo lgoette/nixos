683c6eed998d192c5f62dbfc23487b68cb1c8654
MayNiklas <info@niklas-steffen.de>
2022-12-05 17:27:34 +0100

    add jvmOpts

--- machines/x86_64-linux/minecraft/configuration.nix
+++ machines/x86_64-linux/minecraft/configuration.nix
@@ -33,7 +33,7 @@
       dataDir = "/var/lib/minecraft";
       declarative = true;
       eula = true;
-      jvmOpts = "-Xms2048m -Xmx6656m";
+      jvmOpts = "-Xms2G -Xmx6G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1";
       openFirewall = true;
       serverProperties = {
         enable-rcon = true;
