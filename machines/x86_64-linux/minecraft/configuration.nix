c5df3f1e34c75e768d2c74812cf701241a161a23
Lasse Götte <git@lasse-goette.de>
2022-12-20 02:02:51 +0100

    add hellslime to whitelist

--- machines/x86_64-linux/minecraft/configuration.nix
+++ machines/x86_64-linux/minecraft/configuration.nix
@@ -86,7 +86,7 @@
     whitelist = {
       #BobderEhrenmann = "55df1dd6-8232-47f5-abbf-67c8f49ad26f";
       mineslime2000 = "d6d40e5f-75af-4713-b1fa-522229425116";
-      #hellslime2000 = "41555a0b-9a6f-4596-98eb-d60ed5b02cb3";
+      hellslime2000 = "41555a0b-9a6f-4596-98eb-d60ed5b02cb3";
       #Endslime2000 = "8e82ce9f-80fe-4a23-ab3e-464e0d3776f6";
       #EnderSnow_ = "73341ad1-dabb-4547-b00e-33fb1c488464";
       #hako55 = "df1fc00d-e816-4356-870c-a1492be67740";
