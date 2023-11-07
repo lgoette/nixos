5f46a19a7f1464eb6013b5c015ef4f6aad529429
lgoette <git@lasse-goette.de>
2023-11-07 11:25:36 +0100

    add sd-image output to lamaoutpost

--- machines/lamaoutpost/configuration.nix
+++ machines/lamaoutpost/configuration.nix
@@ -1,12 +1,33 @@
 { self, ... }:
-{ pkgs, lib, mayniklas, home-manager, ... }:
+{ config, pkgs, lib, nixpkgs, nixos-hardware, mayniklas, home-manager, ... }: {
 
-{
+  ### building the image
+  # nix build .#nixosConfigurations.lamaoutpost.config.system.build.sdImage
+
+  # this workaround is currently needed to build the sd-image
+  # basically: there currently is an issue that prevents the sd-image to be built successfully
+  # remove this once the issue is fixed!
+  nixpkgs.overlays = [
+    (final: super: {
+      makeModulesClosure = x:
+        super.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
+
+  nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
+  nix.registry.nixpkgs.flake = nixpkgs;
+  sdImage.compressImage = false;
+  sdImage.imageBaseName = "pi4b-image";
+  ###
+
   imports = [
     self.pi.pi4b
     ../../users/lasse.nix
     ../../users/root.nix
     ./wg0.nix
+
+    "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+    nixos-hardware.nixosModules.raspberry-pi-4
   ];
 
 
