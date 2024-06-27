f7ffacfb662c32b39e15c96f9ec81934bebfb3dc
Lasse Götte <git@lasse-goette.de>
2024-06-27 22:22:24 +0200

    fix sound module

--- modules/sound/default.nix
+++ modules/sound/default.nix
@@ -2,44 +2,26 @@
 with lib;
 let cfg = config.lgoette.sound;
 in {
-  options.lgoette.sound = { enable = mkEnableOption "activate sound with pipewire"; };
-  options.lgoette.sound.pro-audio = { enable = mkEnableOption "activate pro audio environment with jack"; };
-
-  config = mkIf cfg.enable {
-    # Disabled since pipewire is used
-    sound.enable = false;
-    hardware.pulseaudio.enable = false;
-
-    security.rtkit.enable = true;
-
-    # Enable sound with pipewire
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-
-      # Bluetooth configuration
-      wireplumber.configPackages = [
-        #TODO: Wireplumber profileswitching bug not fixed yet: https://gitlab.freedesktop.org/pipewire/wireplumber/-/issues/617
-        # Should be fixed by May 8, 2024
-        (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
-          		bluez_monitor.properties = {
-          			["bluez5.enable-sbc-xq"] = true,
-          			["bluez5.enable-msbc"] = true,
-          			["bluez5.enable-hw-volume"] = true,
-          			["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-          		}
-          	'')
-      ];
-    };
-
+  options.lgoette.sound = { 
+    enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Activate sound with pipewire.
+        '';
+      };
+    pro-audio = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Use low latency audio setup with jack instead of pipewire and realtime kernel.
+        '';
+      };
   };
 
-  config = mkIf cfg.pro-audio.enable {
+  config = mkIf cfg.enable (if cfg.pro-audio then {
 
+    # Disabled since jack is used
     sound.enable = false;
     hardware.pulseaudio.enable = false;
 
@@ -73,5 +55,37 @@ in {
         nameList = "rtc0 snd";
       };
     };
-  };
+
+  } else {
+
+    # Disabled since pipewire is used
+    sound.enable = false;
+    hardware.pulseaudio.enable = false;
+
+    security.rtkit.enable = true;
+
+    # Enable sound with pipewire
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+      # Bluetooth configuration
+      wireplumber.configPackages = [
+        #TODO: Wireplumber profileswitching bug not fixed yet: https://gitlab.freedesktop.org/pipewire/wireplumber/-/issues/617
+        # Should be fixed by May 8, 2024
+        (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+          		bluez_monitor.properties = {
+          			["bluez5.enable-sbc-xq"] = true,
+          			["bluez5.enable-msbc"] = true,
+          			["bluez5.enable-hw-volume"] = true,
+          			["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+          		}
+          	'')
+      ];
+    };
+  });
 }
