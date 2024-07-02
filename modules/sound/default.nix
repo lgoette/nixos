6505d2a8608654d7649c63bf74e47186d59d45c1
Lasse Götte <git@lasse-goette.de>
2024-07-02 23:23:48 +0200

    fix alsa support

--- modules/sound/default.nix
+++ modules/sound/default.nix
@@ -28,7 +28,7 @@ in {
       services.jack = {
         jackd.enable = true;
         # support ALSA only programs via ALSA JACK PCM plugin
-        alsa.enable = true;
+        alsa.enable = false;
         # support ALSA only programs via loopback device (supports programs like Steam)
         loopback = {
           enable = true;
