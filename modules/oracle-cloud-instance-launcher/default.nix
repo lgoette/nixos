200c3ae08b99518886a1487cb22f3f46c59d3832
Lasse Götte <git@lasse-goette.de>
2024-10-15 16:10:59 +0200

    fix yaml reading in oracle module

--- modules/oracle-cloud-instance-launcher/default.nix
+++ modules/oracle-cloud-instance-launcher/default.nix
@@ -21,12 +21,12 @@ in {
     systemd.services."oracle-cloud-instance-launcher" = {
       script = ''
         # Variables defined in /var/src/secrets/oracle/config.yaml
-        COMPARTMENT_ID="$(${pkgs.yq}/bin/yq .COMPARTMENT_ID /var/src/secrets/oracle/config.yaml)"
-        SHAPE="$(${pkgs.yq}/bin/yq .SHAPE /var/src/secrets/oracle/config.yaml)"
-        SUBNET_ID="$(${pkgs.yq}/bin/yq .SUBNET_ID /var/src/secrets/oracle/config.yaml)"
-        DISPLAY_NAME="$(${pkgs.yq}/bin/yq .DISPLAY_NAME /var/src/secrets/oracle/config.yaml)"
-        IMAGE_ID="$(${pkgs.yq}/bin/yq .IMAGE_ID /var/src/secrets/oracle/config.yaml)"
-        SSH_KEYS_FILE="$(${pkgs.yq}/bin/yq .SSH_KEYS_FILE /var/src/secrets/oracle/config.yaml)"
+        COMPARTMENT_ID=$(${pkgs.yq}/bin/yq .COMPARTMENT_ID /var/src/secrets/oracle/config.yaml | xargs)
+        SHAPE=$(${pkgs.yq}/bin/yq .SHAPE /var/src/secrets/oracle/config.yaml | xargs)
+        SUBNET_ID=$(${pkgs.yq}/bin/yq .SUBNET_ID /var/src/secrets/oracle/config.yaml | xargs)
+        DISPLAY_NAME=$(${pkgs.yq}/bin/yq .DISPLAY_NAME /var/src/secrets/oracle/config.yaml | xargs)
+        IMAGE_ID=$(${pkgs.yq}/bin/yq .IMAGE_ID /var/src/secrets/oracle/config.yaml | xargs)
+        SSH_KEYS_FILE=$(${pkgs.yq}/bin/yq .SSH_KEYS_FILE /var/src/secrets/oracle/config.yaml | xargs)
         # AVAILABILITY_DOMAINS # This Variable is used below
 
 
@@ -34,26 +34,26 @@ in {
         launch_instance() {
             local availability_domain=$1
             output=$(${pkgs.oci-cli}/bin/oci compute instance launch \
-                --availability-domain "$availability_domain" \
-                --compartment-id "$COMPARTMENT_ID" \
-                --shape "$SHAPE" \
-                --subnet-id "$SUBNET_ID" \
+                --availability-domain $availability_domain \
+                --compartment-id $COMPARTMENT_ID \
+                --shape $SHAPE \
+                --subnet-id $SUBNET_ID \
                 --assign-private-dns-record true \
                 --assign-public-ip false \
                 --availability-config "{\"recoveryAction\": \"RESTORE_INSTANCE\"}" \
-                --display-name "$DISPLAY_NAME" \
-                --image-id "$IMAGE_ID" \
+                --display-name $DISPLAY_NAME \
+                --image-id $IMAGE_ID \
                 --boot-volume-size-in-gbs 200 \
                 --instance-options "{\"areLegacyImdsEndpointsDisabled\": false}" \
                 --shape-config "{\"ocpus\": 4, \"memoryInGBs\": 24}" \
-                --ssh-authorized-keys-file "$SSH_KEYS_FILE" 2>&1)
+                --ssh-authorized-keys-file $SSH_KEYS_FILE 2>&1)
 
             echo "$output" # Print the output (optional for debugging)
             return $? # Return the exit status
         }
 
         # Main logic
-        output=$(${pkgs.oci-cli}/bin/oci compute instance list --compartment-id "$COMPARTMENT_ID" 2>&1)
+        output=$(${pkgs.oci-cli}/bin/oci compute instance list --compartment-id $COMPARTMENT_ID 2>&1)
 
         # Check if the output is empty or contains no instances
         if [[ -z "$output" || "$output" == *"\"data\": []"* ]]; then
@@ -64,7 +64,7 @@ in {
             exit 1
         fi
 
-        for domain in $(${pkgs.yq}/bin/yq -r '.AVAILABILITY_DOMAINS[]' /var/src/secrets/oracle/config.yaml); do
+        for domain in $(${pkgs.yq}/bin/yq -r '.AVAILABILITY_DOMAINS[]' /var/src/secrets/oracle/config.yaml | xargs); do
             echo "Trying to launch instance in availability domain: $domain"
             output=$(launch_instance "$domain")
 
@@ -73,6 +73,7 @@ in {
                 echo "Error: Out of host capacity. Retrying with the next availability domain..."
             else
                 echo "Instance launched successfully!"
+                echo $output
                 exit 0
             fi
         done
