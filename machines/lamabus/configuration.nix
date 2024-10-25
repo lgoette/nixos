2bb2d916bbee03cffbeb1cb08a1364dfcf989c48
Lasse Götte <git@lasse-goette.de>
2024-10-25 12:28:17 +0200

    fix intel firmware missing

--- machines/lamabus/configuration.nix
+++ machines/lamabus/configuration.nix
@@ -223,6 +223,7 @@ in {
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.enableRedistributableFirmware = config.nixpkgs.config.allowUnfree;
   system.stateVersion = "22.05";
 
 }
