3d8017a8fb56bec75b05c6e99c967d199cdd745c
MayNiklas <info@niklas-steffen.de>
2024-12-08 21:00:08 +0100

    downgrade mixxx to 2.4.1

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,7 +4,8 @@ let
   # instead of having to keep sha256 hashes in each package for src
   inherit inputs;
 in
-self: super: {
+self: super:
+let system = super.system; in {
   # Custom packages. Will be made available on all machines and used where
   # needed.
 
@@ -17,5 +18,13 @@ self: super: {
 
   # inherit (super.pkgs.callPackages ../packages/unifi { }) unifiLTS unifi5 unifi6 unifi7;
   # unifi = super.pkgs.unifi7;
+
+  # downgrade mixxx to 2.4.1
+  mixxx = (import
+    (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/05bbf675397d5366259409139039af8077d695ce.tar.gz";
+      sha256 = "sha256:1r26vjqmzgphfnby5lkfihz6i3y70hq84bpkwd43qjjvgxkcyki0";
+    })
+    { inherit system; }).mixxx;
 
 }
