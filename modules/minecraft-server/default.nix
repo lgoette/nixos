a30a27505cab46904c71ad55fced83d5925e74a6
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-11-16 15:05:41 +0100

    fix bluemap source

--- modules/minecraft-server/default.nix
+++ modules/minecraft-server/default.nix
@@ -36,8 +36,8 @@ in {
 
           symlinks = {
             "plugins/bluemap-5.13-paper.jar" = pkgs.fetchurl {
-              url = "https://cdn.modrinth.com/data/swbUV1cr/versions/KDFOHrSO/bluemap-5.13-paper.jar";
-              sha512 = "858805ca7187216b82817fb3e697a9d5bfb8d215f399dee653f9152bea4b6292d1d271c6287195cafa53cdec774e964e8a6d6a7ed018e397e72525d102c4dc0c";
+              url = "https://cdn.modrinth.com/data/swbUV1cr/versions/wpE4tHiK/bluemap-5.13-paper.jar";
+              hash = "sha256-KkMLP09ZZN+Ev4WhRp4BA6l+applaGAJbwurzwTsUqc=";
             };
           };
         };
